0fb5dd37728cc9cdd3bcb91c021df39d617cc501
Fer3Orenday <fer.orenday3@gmail.com>
2022-12-04 14:08:51 -0600

    Empece con el encabezado y pagina de incio

---
+++ inicio.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+
+    <!-- LINK bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <!-- link estilos -->
+    <link rel="stylesheet" href="./css/estilosEncabezado.css">
+    <!-- link fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Concert+One&family=Xanh+Mono:ital@1&display=swap" rel="stylesheet">
+    <!-- link fontawesome -->
+    <script src="https://kit.fontawesome.com/25e2610697.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <!-------------------- ENCABEZADO PARTE1 ----------------->
+
+
+    <nav class="navbar navbar-expand " class="encabezado" style="background-color:rgb(224, 240, 224);">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="opcionesM2 opcion1">
+                    <a class="nav-link active" aria-current="page" href="#">Opcion1</a>
+
+                </li>
+
+                <li class="opcionesM2">
+                    <a class="nav-link active" aria-current="page" href="#">-</a>
+                </li>
+
+
+                <li class="opcionesM2">
+                    <a class="nav-link active" aria-current="page" href="#">Ayuda</a>
+                </li>
+
+                <li class="opcionesM2">
+                    <a class="nav-link active" aria-current="page" href="#">-</a>
+                </li>
+
+                <li class="opcionesM2">
+                    <a class="nav-link active" aria-current="page" href="#">Iniciar Sesión</a>
+                </li>
+            </ul>
+    </nav>
+
+
+    <!-------------------- ENCABEZADO PARTE 2 ----------------->
+    <nav class="navbar navbar-expand-lg navbar-light " style="background-color:rgb(153, 190, 153); ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="./imagenes/logo.png" alt="" width="40%" height="30%" >
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav" text-align: center;>
+                <ul class="navbar-nav">
+                    <li class="opcionesM">
+                        <a class="nav-link active" aria-current="page" href="#">Opcion1</a>
+                    </li>
+                    <li class="opcionesM">
+                        <a class="nav-link active" aria-current="page" href="#">Opcion2</a>
+                    </li>
+                    <li class="opcionesM">
+                        <a class="nav-link active" aria-current="page" href="#">Opcion3</a>
+                    </li>
+                    <li class="opcionesM">
+                        <a class="nav-link active" aria-current="page" href="#">Opcion4</a>
+                    </li>
+                    <li class="opcionesM">
+                        <a class="nav-link active" aria-current="page" href="#">Opcion5</a>
+                    </li>
+                    <li class="opcionesM">
+                        <a class="nav-link active" aria-current="page" href="#">Opcion6</a>
+                    </li>
+
+                </ul>
+
+                <form class="d-flex" style="text-align: center; padding-left: 30px;">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+
+                <a href="https://www.facebook.com/"><i style="color: black; padding-left: 33px;" class="fa-solid fa-cart-shopping fa-3x "></i></a>
+            </div>
+        </div>
+    </nav>
+
+    <!-------------------- ENCABEZADO PARTE 3 ----------------->
+
+    <nav class="navbar navbar-expand " class="encabezado" style="background-color:rgb(224, 240, 224);">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="opcionesM2 opcion2">
+                    <a class="nav-link active" aria-current="page" href="#">¡Nuevos lanzamientos cada dia!</a>
+                </li>
+            </ul>
+    </nav>
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
