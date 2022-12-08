c506dd7855b1bcaf88030a03d5c17ac07a48a132
Fer3Orenday <fer.orenday3@gmail.com>
2022-12-07 19:51:00 -0600

    Footer

--- inicio.php
+++ inicio.php
@@ -23,7 +23,7 @@
     <!-------------------- ENCABEZADO PARTE1 ----------------->
 
 
-    <nav class="navbar navbar-expand " class="encabezado" style="background-color: rgb(217, 217, 235);  ">
+    <nav class="navbar navbar-expand " class="encabezado" style="background-color: rgb(217, 217, 235);  padding-left: 600px; ">
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="opcionesM2 opcion1">
@@ -109,13 +109,13 @@
 
 
     <!-- VIDEO DE INICIO  -->
-    
-        <video width="100%"  controls>
-            <source src="./imagenes/video.mp4" type="video/mp4">
 
-            Your browser does not support HTML video.
-        </video>
-    
+    <video width="100%" controls>
+        <source src="./imagenes/video.mp4" type="video/mp4">
+
+        Your browser does not support HTML video.
+    </video>
+
 
     <!-- 3 IMAGENES  -->
     <div class="card-group">
@@ -144,6 +144,106 @@
             </div>
         </div>
     </div>
+
+
+    <!-- PIE DE PAGINA -->
+
+    <div >
+        <!-- Footer -->
+        <footer class="text-center text-lg-start text-black" style="background-color: #929fba">
+            <!-- Grid container -->
+            <div class="container p-4 pb-0">
+                <!-- Section: Links -->
+                <section class="">
+                    <!--Grid row-->
+                    <div class="row">
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">
+                                Drokey
+                            </h6>
+                            <p>
+                                Siendo la mejor compañia de tienda deportiva desde 2014, brindamos 
+                                la mejor calidad en nuestros productos. Siempre acompañandote a donde
+                                vayas. 
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <hr class="w-100 clearfix d-md-none" />
+
+                        <!-- Grid column -->
+                        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">Sucursales</h6>
+                            <p>
+                                <a class="text-black">España</a>
+                            </p>
+                            <p>
+                                <a class="text-black">México</a>
+                            </p>
+                            <p>
+                                <a class="text-black">Argentina</a>
+                            </p>
+                            <p>
+                                <a class="text-black">Dubai</a>
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <hr class="w-100 clearfix d-md-none" />
+
+                        <!-- Grid column -->
+                        <hr class="w-100 clearfix d-md-none" />
+
+                        <!-- Grid column -->
+                        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">Contacto</h6>
+                            <p><i class="fas fa-home mr-3"></i> Aguascalientes, Mexico CP 20298</p>
+                            <p><i class="fas fa-envelope mr-3"></i> drokey@gmail.com</p>
+                            <p><i class="fas fa-phone mr-3"></i> + 52 449 98 78 65</p>
+                            <p><i class="fas fa-print mr-3"></i> + 52 449 763 65 24</p>
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">Siguenos</h6>
+
+                            <!-- Facebook -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+
+                            <!-- Twitter -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+
+                            <!-- Google -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39" href="#!" role="button"><i class="fab fa-google"></i></a>
+
+                            <!-- Instagram -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+
+                            <!-- Linkedin -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #0082ca" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+                            <!-- Github -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #333333" href="#!" role="button"><i class="fab fa-github"></i></a>
+                        </div>
+                    </div>
+                    <!--Grid row-->
+                </section>
+                <!-- Section: Links -->
+            </div>
+            <!-- Grid container -->
+
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+                © 2022 Company:
+                <a class="text-white" href="https://mdbootstrap.com/">Drokey</a>
+            </div>
+            <!-- Copyright -->
+        </footer>
+        <!-- Footer -->
+    </div>
+    <!-- End of .container -->
+
 
 
 
