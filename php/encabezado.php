18fbdf8b7396ad2c05a1c8fc23b22b80f3e79cc4
Fer3Orenday <fer.orenday3@gmail.com>
2022-12-07 23:24:16 -0600

    encabezado

--- php/encabezado.php
+++ php/encabezado.php
@@ -21,12 +21,14 @@
 
     <!-------------------- ENCABEZADO PARTE1 ----------------->
 
+<!-------------------- ENCABEZADO PARTE1 ----------------->
 
-    <nav class="navbar navbar-expand " class="encabezado" style="background-color:rgb(217, 217, 235);">
+
+<nav class="navbar navbar-expand " class="encabezado" style="background-color: rgb(217, 217, 235);  padding-left: 500px; ">
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="opcionesM2 opcion1">
-                    <a class="nav-link active" aria-current="page" href="#">Opcion1</a>
+                    <a class="nav-link active" aria-current="page" href="#">hola</a>
 
                 </li>
 
@@ -36,7 +38,7 @@
 
 
                 <li class="opcionesM2">
-                    <a class="nav-link active" aria-current="page" href="#">Ayuda</a>
+                    <a class="nav-link active" aria-current="page" href="#">Acerca de</a>
                 </li>
 
                 <li class="opcionesM2">
@@ -51,50 +53,52 @@
 
 
     <!-------------------- ENCABEZADO PARTE 2 ----------------->
-    <nav class="navbar navbar-expand-lg navbar-light " style="background-color:rgb(171, 171, 218); ">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="../imagenes/logo.png" alt="" width="40%" height="30%" >
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav" text-align: center;>
-                <ul class="navbar-nav">
-                    <li class="opcionesM">
-                        <a class="nav-link active" aria-current="page" href="#">Opcion1</a>
-                    </li>
-                    <li class="opcionesM">
-                        <a class="nav-link active" aria-current="page" href="#">Opcion2</a>
-                    </li>
-                    <li class="opcionesM">
-                        <a class="nav-link active" aria-current="page" href="#">Opcion3</a>
-                    </li>
-                    <li class="opcionesM">
-                        <a class="nav-link active" aria-current="page" href="#">Opcion4</a>
-                    </li>
-                    <li class="opcionesM">
-                        <a class="nav-link active" aria-current="page" href="#">Opcion5</a>
-                    </li>
-                    <li class="opcionesM">
-                        <a class="nav-link active" aria-current="page" href="#">Opcion6</a>
-                    </li>
+    <header style="position: sticky ; top: 0px; z-index: 1000;">
+        <nav class="navbar navbar-expand-lg navbar-light " style="background-color: rgb(171, 171, 218); position: sticky; ">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="../imagenes/logo.png" alt="" width="40%" height="30%">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="opcionesM">
+                            <a class="nav-link active" aria-current="page" href="#">Opcion1</a>
+                        </li>
+                        <li class="opcionesM">
+                            <a class="nav-link active" aria-current="page" href="#">Opcion2</a>
+                        </li>
+                        <li class="opcionesM">
+                            <a class="nav-link active" aria-current="page" href="#">Opcion3</a>
+                        </li>
+                        <li class="opcionesM">
+                            <a class="nav-link active" aria-current="page" href="#">Nosotros</a>
+                        </li>
+                        <li class="opcionesM">
+                            <a class="nav-link active" aria-current="page" href="#">Contáctanos</a>
+                        </li>
+                        <li class="opcionesM">
+                            <a class="nav-link active" aria-current="page" href="#">Ayuda</a>
+                        </li>
 
-                </ul>
+                    </ul>
 
-                <form class="d-flex" style="text-align: center; padding-left: 30px;">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+                    <form class="d-flex" style="text-align: center; padding-left: 30px;">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
 
-                <a href="https://www.facebook.com/"><i style="color: black; padding-left: 33px;" class="fa-solid fa-cart-shopping fa-3x "></i></a>
+                    <a href="https://www.facebook.com/"><i style="color: black; padding-left: 33px;" class="fa-solid fa-cart-shopping fa-3x "></i></a>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
+    </header>
 
     <!-------------------- ENCABEZADO PARTE 3 ----------------->
 
-    <nav class="navbar navbar-expand " class="encabezado" style="background-color:rgb(217, 217, 235);">
+    <nav class="navbar navbar-expand " class="encabezado" style="background-color:rgb(217, 217, 235); position: sticky;">
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="opcionesM2 opcion2">
@@ -102,6 +106,13 @@
                 </li>
             </ul>
     </nav>
+
+
+
+
+
+
+    
 
 </body>
 
