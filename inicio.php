66685c4bccdc853cb85806ac9a198398838477a2
acdlpg <gallegoscarolina61@gmail.com>
2022-12-10 11:48:58 -0600

    se agrego el carrusel

--- inicio.php
+++ inicio.php
@@ -149,6 +149,44 @@
     <br>
     <br>
 
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./imagenes/a1.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Calzado Adidas de Dama</h5>
+        <p>Nueva Coleccion</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./imagenes/a2.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Calzado Reebok de Caballero</h5>
+        <p>Nueva Coleccion</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./imagenes/a4.jpeg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Calzado Unisex</h5>
+        <p>Nueva Coleccion</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
 
     <div>
