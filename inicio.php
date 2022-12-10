d84a5b928380abda8f22a763b35aabae6da90938
Fer3Orenday <fer.orenday3@gmail.com>
2022-12-10 00:36:54 -0600

    inicio

--- inicio.php
+++ inicio.php
@@ -12,8 +12,12 @@
     <!-- link estilos -->
     <link rel="stylesheet" href="./css/estilosEncabezado.css">
     <link rel="stylesheet" href="./css/estilosImagen.css">
+    <link rel="stylesheet" href="./css/estilosPagInicio.css">
     <!-- link fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Concert+One&family=Xanh+Mono:ital@1&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dangrek&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mouse+Memoirs&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mouse+Memoirs&display=swap" rel="stylesheet">
     <!-- link fontawesome -->
     <script src="https://kit.fontawesome.com/25e2610697.js" crossorigin="anonymous"></script>
 </head>
@@ -21,7 +25,6 @@
 <body style="background-color: rgb(253, 253, 246);">
 
     <!-------------------- ENCABEZADO PARTE1 ----------------->
-
 
     <nav class="navbar navbar-expand " class="encabezado" style="background-color: rgb(217, 217, 235);  padding-left: 400px; ">
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -101,10 +104,14 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="opcionesM2 opcion2">
-                    <a class="nav-link active" aria-current="page" href="#">¡Nuevos lanzamientos cada dia!</a>
+                    <a class="nav-link active" aria-current="page" href="#" style="padding-left: 430px;">¡Nuevos lanzamientos cada dia!</a>
                 </li>
             </ul>
     </nav>
+
+
+
+
 
 
 
@@ -122,49 +129,38 @@
         <div class="card">
             <img src="./imagenes/img1.jpg" class="card-img-top imagen2" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text imgss">Seguridad en cada paso</p>
 
             </div>
         </div>
         <div class="card">
             <img src="./imagenes/img2.webp" class="card-img-top imagen2" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text imgss">Todo para mejorar tu rendimiento.</p>
 
             </div>
         </div>
         <div class="card">
             <img src="./imagenes/img3.jpg" class="card-img-top imagen2" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <p class="card-text imgss">Los tenis del momento</p>
 
             </div>
         </div>
     </div>
     <br>
-    <br>
-    <br>
 
-
-
-    <div>
-        <div class="hero-image">
-            <div class="hero-text">
-                <h1 style="font-size:50px">I am John Doe</h1>
-                <p>And I'm a Photographer</p>
-                <button>Hire me</button>
-            </div>
-        </div>
+    <div style="background-image: url('./imgsInicio/fondoNavidad.jpg');" >
+        <h1 class="fondNavidad">
+            <br>
+            Para esta navidad 
+            <br>
+            Regala Drokey
+            <br>
+            <br>
+         
+        </h1>
     </div>
-
-
-
-
-
-    <br>
 
 
     <!-- PIE DE PAGINA -->
